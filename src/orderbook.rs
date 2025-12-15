1dc86334fec6a7e0db6a3d266b1b2bd2838f54d7
Winnerty <timofey.n.karpeev@gmail.com>
2025-12-15 14:34:01 +0100

    moved from Vec to BTreeMap

--- src/orderbook.rs
+++ src/orderbook.rs
@@ -1,58 +1,42 @@
+use std::collections::BTreeMap;
 use crate::order::{Order, Side};
 
 pub struct OrderBook {
-    pub bids: Vec<(u64, u64)>, //bids are ordered DESC price
-    pub asks: Vec<(u64, u64)>, //asks are ordered ASC price
+    pub bids: BTreeMap<u64, u64>,
+    pub asks: BTreeMap<u64, u64>,
 }
 
 impl OrderBook {
     pub fn new() -> Self {
         Self {
-            bids: vec![],
-            asks: vec![]
+            bids: BTreeMap::new(),
+            asks: BTreeMap::new(),
         }
     }
 
     pub fn snapshot(&self, amount: usize) -> (Vec<(u64, u64)>, Vec<(u64, u64)>) {
-        let bid_depth: usize = amount.min(self.bids.len());
-        let ask_depth: usize = amount.min(self.asks.len());
-        
-        (self.bids.iter().cloned().take(bid_depth).collect(), self.asks.iter().cloned().take(ask_depth).collect())
+        let bids: Vec<(u64, u64)> = self.bids.iter().rev().take(amount).map(|(p, q)| (*p, *q)).collect();
+        let asks: Vec<(u64, u64)> = self.asks.iter().take(amount).map(|(p, q)| (*p, *q)).collect();
+
+        (bids, asks)
     }
 
     pub fn spread(&self) -> Option<u64> {
-        if self.bids.is_empty() || self.asks.is_empty() {
-            return None;
-        }
+        let best_ask: &u64 = self.asks.iter().next()?.0;
+        let best_bid: &u64 = self.bids.iter().rev().next()?.0;
 
-        Some(self.asks[0].0 - self.bids[0].0)
+         Some(best_ask - best_bid)
     }
 
     pub fn proceed_order(&mut self, new_order: Order) {    
         match new_order.side {
             Side::Buy => {
-                for i in 0..self.bids.len() {
-                    if new_order.price > self.bids[i].0 {
-                        self.bids.insert(i, (new_order.price, new_order.quantity));
-                        return;
-                    } else if new_order.price == self.bids[i].0 {
-                        self.bids[i].1 += new_order.quantity;
-                        return;
-                    }
-                }
-                self.bids.push((new_order.price, new_order.quantity));
+                let entry: &mut u64 = self.bids.entry(new_order.price).or_insert(0);
+                *entry += new_order.quantity;
             }
             Side::Sell => {
-                for i in 0..self.asks.len() {
-                    if new_order.price < self.asks[i].0 {
-                        self.asks.insert(i, (new_order.price, new_order.quantity));
-                        return;
-                    } else if new_order.price == self.asks[i].0 {
-                        self.asks[i].1 += new_order.quantity;
-                        return;
-                    }
-                }
-                self.asks.push((new_order.price, new_order.quantity));
+                let entry: &mut u64 = self.asks.entry(new_order.price).or_insert(0);
+                *entry += new_order.quantity;
             }
         }
     }
